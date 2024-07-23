3b5a1fb9a423e65e7eeb7eec7259197066e53a36
Jerônimo <jeronimo@testes-MacBook-Pro.local>
2024-07-23 14:15:12 -0300

    Continuacao do dash

--- src/components/Dashboard/DashADM/index.tsx
+++ src/components/Dashboard/DashADM/index.tsx
@@ -1,5 +1,5 @@
-import { BoxCount, Typografy } from "@mtfu/react";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { Avatar, BoxCount, Typografy } from "@mtfu/react";
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, LineChart, Line, DefaultTooltipContent, PieChart, Pie, Cell } from 'recharts';
 
 import {
     Select,
@@ -17,114 +17,90 @@ import { Separator } from "@/components/ui/separator";
 
 const data = [
     {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
+      name: '21/07',
+      pontos: 4,
+      
     },
     {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
+      name: '22/07',
+      pontos: 3,
+      
     },
     {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
+      name: '23/07',
+      pontos: 2,
+      
     },
     {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
+      name: '24/07',
+      pontos: 1,
+      
     },
     {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
+      name: '25/07',
+      pontos: 1,
+      
     },
     {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
+      name: '26/07',
+      pontos: 2,
+      
     },
     {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
+      name: '27/07',
+      pontos: 3,
+      
     },
   ];
 
   const dataPointDelivery = [
     {
-      name: '22/07/2024',
-      pts: 10,
-      pv: 2400,
-      amt: 2400,
+      name: 'Sprint 1',
+      pontos: 4,
+      
     },
     {
-      name: '23/07/2024',
-      pts: -0,
-      pv: -1398,
-      amt: -2210,
+      name: 'Sprint 2',
+      pontos: 3,
+      
     },
     {
-      name: '24/07/2024',
-      pts: 3,
-      pv: 9800,
-      amt: 2290,
+      name: 'Sprint 3',
+      pontos: 2,
+      
     },
     {
-      name: '25/07/2024',
-      pts: 4,
-      pv: 3908,
-      amt: 2000,
+      name: 'Sprint 4',
+      pontos: 1,
+      
     },
     {
-      name: '26/07/2024',
-      pts: 5,
-      pv: 4800,
-      amt: 2181,
+      name: 'Sprint 5',
+      pontos: 1,
+      
     },
     {
-      name: '27/07/2024',
-      pts: 0,
-      pv: 3800,
-      amt: 2500,
+      name: 'Sprint 6',
+      pontos: 2,
+      
     },
     {
-      name: '28/07/2024',
-      pts: 0,
-      pv: 4300,
-      amt: 2100,
+      name: 'Sprint 7',
+      pontos: 3,
+      
     },
   ];
+
+  const dataPie = [
+    { name: 'Entregues', value: 400 },
+    { name: 'A fazer', value: 200 },
+  ];
   
-  
+  const COLORS = ['#6111CA', '#222222'];
 
 
 export function DashADM(){
-    
-    const gradientOffset = () => {
-        const dataMax = Math.max(...data.map((i) => i.uv));
-        const dataMin = Math.min(...data.map((i) => i.uv));
-      
-        if (dataMax <= 0) {
-          return 0;
-        }
-        if (dataMin >= 0) {
-          return 1;
-        }
-      
-        return dataMax / (dataMax - dataMin);
-      };
 
-    const off = gradientOffset();
 
     return(
             <main className='w-full  flex '>
@@ -151,10 +127,13 @@ export function DashADM(){
                         </div>
                     </div>
 
-                    <div className='w-full md:h-32 lg:h-52 mt-8 py-5 pl-6  pr-6'>
+                    <div className='w-full md:h-32 lg:h-52 xl:h-42 2xl:h-72 lg:mt-8 xl:mt-0 py-5 pl-6  pr-6'>
                             <ResponsiveContainer width="100%" height="100%">
-                                <BarChart width={150} height={300} data={data}>
-                                    <Bar dataKey="uv" fill={colors.mtfu} />
+                                <BarChart width={150} height={300} data={dataPointDelivery}>
+                                    <Bar dataKey="pontos" fill={colors.mtfu} />
+                                    <XAxis dataKey="name" fontSize={10} />
+                                          <YAxis fontSize={10}/>
+                                          <DefaultTooltipContent/>
                                 </BarChart>
                             </ResponsiveContainer>
                     </div>
@@ -182,47 +161,116 @@ export function DashADM(){
                                 </div>
                             </div>
                         </div>
+
                         <div className="flex py-1 pl-10 pr-8 w-full gap-14">
-                            <div className="bg-gray_fundo_mtfu w-1/3 h-52 rounded-xl pl-4 py-2 items-start justify-start">
+                            <div className="bg-gray_fundo_mtfu w-1/3 lg:h-52 2xl:h-72 rounded-xl pl-4 py-2 items-start justify-start">
                                 <Typografy align="left" children="Pontos entregues (7 dias) " color="#878787" fontWeight={400} type="footer" />
 
-                                <div className="w-full h-40 py-2 pr-4">
-                                    <ResponsiveContainer width="100%" height="100%">
-                                        <AreaChart
-                                        width={500}
-                                        height={400}
-                                        data={dataPointDelivery}
-                                        margin={{
-                                            top: 10,
-                                            right: 0,
-                                            left: -30,
-                                            bottom: 0,
-                                        }}
-                                        
+                                <div className="w-full lg:h-44 sm:h-30 py-2 pr-6 ">
+                                  <ResponsiveContainer width="100%" height="100%">
 
-                                        >
-                                        <CartesianGrid strokeDasharray="0 0"  />
-                                        <XAxis dataKey="name" />
-                                        <YAxis />
-                                        <Tooltip />
-                                        <defs>
-                                            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                                            <stop offset={off} stopColor={colors.mtfu} stopOpacity={1} />
-                                            <stop offset={off} stopColor="red" stopOpacity={1} />
-                                            </linearGradient>
-                                        </defs>
-                                        <Area type="natural" dataKey="pts" stroke="" fill="url(#splitColor)" activeDot={true}
-                                        />
-                                        </AreaChart>
-                                    </ResponsiveContainer>
+                                    <LineChart width={300} height={100} data={data} 
+                                    margin={{
+                                      top: 10,
+                                      right: 0,
+                                      left: -40,
+                                      bottom: 0,
+                                    }} >
+                                      <Line type="monotone" dataKey="pontos" stroke={colors.mtfu} strokeWidth={2}
+                                       dot={false}/>
+                                        <XAxis dataKey="name" fontSize={10} />
+                                          <YAxis fontSize={10}/>
+                                      <Tooltip />
+                                    </LineChart>
+
+                                  </ResponsiveContainer>
+                                  
                                 </div>
                             
                             </div>
-                            <div className="bg-gray_fundo_mtfu w-1/3 h-52 rounded-xl pl-4 py-2">
+                            <div className="bg-gray_fundo_mtfu w-1/3 lg:h-52 2xl:h-72 rounded-xl pl-4 py-2">
                                 <Typografy align="left" children="Conclusao geral " color="#878787" fontWeight={400} type="footer" />
+                            
+                                <div className="w-full lg:h-44 sm:h-30 py-0 pr-6 ">
+                                  <ResponsiveContainer width="100%" height="100%">
+                                      <PieChart width={400} height={400} >
+                                        <Pie
+                                          data={dataPie}
+                                          cx="50%"
+                                          cy="50%"
+                                          innerRadius={40}
+                                          outerRadius={75}
+                                          fill="#000000"
+                                          paddingAngle={0}
+                                          dataKey="value"
+
+                                        >
+                                         
+                                          {data.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} stroke="#121212"/>
+                                          ))}
+                                        </Pie>
+                                        <Tooltip />
+                                      </PieChart>
+                                  </ResponsiveContainer>
+                                </div>
+                                  
                             </div>
-                            <div className="bg-gray_fundo_mtfu w-1/3 h-52 rounded-xl pl-4 py-2">
+                            <div className="bg-gray_fundo_mtfu w-1/3 lg:h-52 2xl:h-72 rounded-xl pl-4 py-2">
                                 <Typografy align="left" children="Pontos por usuario " color="#878787" fontWeight={400} type="footer" />
+                            
+                                <div className="flex flex-col mt-3 max-h-40 overflow-scroll bg-local gap-4">
+
+                                  <div className="flex gap-2">
+                                    <div className="flex items-center justify-center">
+                                      <Avatar title="Nicolas" src="github.com/njeronimo1.png" alt="" />
+                                    </div>
+                                    <div className="flex w-full flex-col gap-1">
+                                      <Typografy align="left" children="Nicolas Jeronimo" color="#878787" fontWeight={400} type="medium" />
+                                      <Typografy align="left" children="26pts " color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                  </div>
+
+                                  <div className="flex gap-2">
+                                    <div className="flex items-center justify-center">
+                                      <Avatar title="Nicolas" src="github.com/njeronimo1.png" alt="" />
+                                    </div>
+                                    <div className="flex w-full flex-col gap-1">
+                                      <Typografy align="left" children="Nicolas Jeronimo" color="#878787" fontWeight={400} type="medium" />
+                                      <Typografy align="left" children="26pts " color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                  </div>
+
+                                  <div className="flex gap-2">
+                                    <div className="flex items-center justify-center">
+                                      <Avatar title="Nicolas" src="github.com/njeronimo1.png" alt="" />
+                                    </div>
+                                    <div className="flex w-full flex-col gap-1">
+                                      <Typografy align="left" children="Nicolas Jeronimo" color="#878787" fontWeight={400} type="medium" />
+                                      <Typografy align="left" children="26pts " color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                  </div>
+
+                                  <div className="flex gap-2">
+                                    <div className="flex items-center justify-center">
+                                      <Avatar title="Nicolas" src="github.com/njeronimo1.png" alt="" />
+                                    </div>
+                                    <div className="flex w-full flex-col gap-1">
+                                      <Typografy align="left" children="Nicolas Jeronimo" color="#878787" fontWeight={400} type="medium" />
+                                      <Typografy align="left" children="26pts " color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                  </div>
+
+                                  <div className="flex gap-2">
+                                    <div className="flex items-center justify-center">
+                                      <Avatar title="Nicolas" src="github.com/njeronimo1.png" alt="" />
+                                    </div>
+                                    <div className="flex w-full flex-col gap-1">
+                                      <Typografy align="left" children="Nicolas Jeronimo" color="#878787" fontWeight={400} type="medium" />
+                                      <Typografy align="left" children="26pts " color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                  </div>
+                                </div>
                             </div>
                         </div>
                     </div>
