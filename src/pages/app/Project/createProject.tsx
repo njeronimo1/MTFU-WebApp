580b7c6972159b43378c46535136366c2979b47f
Nicolas Gonçalves Jerônimo <ngjeronimo1@gmail.com>
2024-07-30 17:03:28 -0300

    sync

--- src/pages/app/Project/createProject.tsx
+++ src/pages/app/Project/createProject.tsx
@@ -1,9 +1,11 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb";
+import { useForm, SubmitHandler } from "react-hook-form"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 import { Button, Input, TextArea, Typografy } from "@mtfu/react";
 import { useState } from "react";
 import { UsersList } from "./projectTypes";
+import { z } from 'zod'
 
 import {
     Table,
@@ -18,7 +20,28 @@ import {
 import { Trash } from "phosphor-react";
 
 
+const createProjectForm = z.object({
+    title: z.string(),
+    responsibleId: z.number(),
+    categoryId: z.number(),
+    users: z.string().optional().array()
+})
+
+type CreateProjectForm = z.infer<typeof createProjectForm>
+
 export function CreateProject(){
+
+    const { register, handleSubmit, formState: {isSubmitting, errors} } = useForm<CreateProjectForm>({
+        defaultValues: {
+            users: []
+        }
+    });
+
+    console.log(errors.root);
+
+    function handleSubmitProject(data: CreateProjectForm){
+        console.log(data);
+    }
 
     const [usersList, setUsersList] = useState<UsersList[]>([
         {   
@@ -58,15 +81,25 @@ export function CreateProject(){
 
             <Separator className="bg-separator_app" />
 
-            <div className="w-full p-8">
-                <div className="w-full flex gap-4">
+            <form className="w-full p-8" onSubmit={handleSubmit(handleSubmitProject)}>
+                <div className="w-full flex gap-4" >
                     <div className="w-1/4">
-                        <Input label="Titulo" type="text" optional={false} placeholder="Preencha aqui..." variant="text" onChange={() => {}} errorMessage="" imgSearch="" />
+                        <Input 
+                            label="Titulo" 
+                            type="text" 
+                            optional={false} 
+                            placeholder="Preencha aqui..." 
+                            variant="text" 
+                            // onChange={() => {}}
+                            {...register("title")} 
+                            errorMessage="" 
+                            imgSearch="" 
+                        />
                     </div>
                     
                     <div className="w-1/4 flex flex-col gap-[0.5rem]">
                         <Typografy fontWeight={600} type="medium" color="white" children="Responsável" align="left"  />
-                        <Select>
+                        <Select {...register("responsibleId")} >
                             <SelectTrigger className="w-full h-12 bg-gray_fundo_sec_mtfu text-white border-mtfu hover:bg-mtfu focus:ring-mtfu focus:ring-offset-3">
                                 <SelectValue placeholder="Selecione um responsável" />
                             </SelectTrigger>
@@ -80,7 +113,7 @@ export function CreateProject(){
 
                     <div className="w-1/4 flex flex-col gap-[0.5rem]">
                         <Typografy fontWeight={600} type="medium" color="white" children="Categoria" align="left"  />
-                        <Select>
+                        <Select {...register("categoryId")} >
                             <SelectTrigger className="w-full h-12 bg-gray_fundo_sec_mtfu text-white border-mtfu hover:bg-mtfu focus:ring-mtfu focus:ring-offset-3">
                                 <SelectValue placeholder="Selecione uma categoria" />
                             </SelectTrigger>
@@ -106,12 +139,11 @@ export function CreateProject(){
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
-                        </div>
                     </div>
                 </div>
 
                 {usersList.length > 0 && (
-                    <div className="w-full pl-8 pr-8 flex flex-col">
+                    <div className="w-full py-4 flex flex-col">
                         <Table>
                             <TableHeader>
                                 <TableRow className="hover:bg-transparent border-b-separator_app">
@@ -139,18 +171,19 @@ export function CreateProject(){
                     </div>
                 )}
 
-                <div className="w-full p-8 text-white flex gap-6 flex-col">
+                <div className="w-full py-4 text-white flex gap-6 flex-col">
                     <div className="w-full">
                         <TextArea label="Descrição" onChange={() => {}} placeholder="Preencha aqui..." value="" errorMessage="" />
                     </div>
                     
-                    <div className="w-full flex items-end justify-end">
-                        <Button  variant="normal" textAlign="center" radius="8" >
+                    <div className="w-full flex items-end justify-end" >
+                        <Button  variant="normal" textAlign="center" radius="8" type="submit" >
                             Criar
                         </Button>
                     </div>
                     
                 </div>
+            </form>
         </>
     )
 }
