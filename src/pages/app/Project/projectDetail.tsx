cfccba1e4429ff679ddf1fb7c5398714c4cbbae0
Jerônimo <jeronimo@testes-MacBook-Pro.local>
2024-08-20 06:34:35 -0300

    Header do detalhe de projeto, com grafico

--- src/pages/app/Project/projectDetail.tsx
+++ src/pages/app/Project/projectDetail.tsx
@@ -3,11 +3,29 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrig
 import { Separator } from "@/components/ui/separator";
 import { Avatar, Status, Typografy } from "@mtfu/react";
 import { UserPlus } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+
+const dataPie = [
+    { name: 'Entregues', value: 400 },
+    { name: 'A fazer', value: 200 },
+  ];
+  
+  const COLORS = ['#FFF', '#222222'];
 
 export function ProjectDetail(){
 
     const { id } = useParams();
+
+    const [conclusionProject, setConclusionProject] = useState('');
+
+    useEffect(() => {
+        let total = dataPie[0].value + dataPie[1].value;
+        let percentage = (dataPie[0].value / total) * 100;
+
+        setConclusionProject(percentage.toFixed(0) + '%');
+    },[]);
 
     return (
         <>
@@ -27,7 +45,7 @@ export function ProjectDetail(){
             <Separator className="bg-separator_app" />
 
             <div className="flex w-full">
-                <div className="pl-10 py-4 w-1/3 border-r border-separator_app pr-4">
+                <div className="pl-4 py-4 w-1/3 border-r border-separator_app pr-4">
                     <div>
                         <Typografy align="left" children="Projeto teste" color="white" fontWeight={500} type="title" />
                         <div className="flex py-2">
@@ -56,12 +74,12 @@ export function ProjectDetail(){
                             </div>
                         </div>
                     </div>
-                    <div className="flex w-full gap-2 mt-2">
-                        <div className="bg-gray_fundo_mtfu pl-6 py-4 pr-4 pb-4 rounded-sm w-1/2">
+                    <div className="flex w-full gap-2 mt-2 h-[7.5rem] cursor-default">
+                        <div className="flex flex-col bg-gray_fundo_mtfu pl-6 py-8 pr-4 pb-4 rounded-sm w-1/2">
                             <Typografy align="left" children="3" color="white" fontWeight={600} type="title" />
                             <Typografy align="left" children="Total sprints" color="white" fontWeight={400} type="medium" />
                         </div>
-                        <div className="bg-gray_fundo_mtfu pl-6 py-4 pr-4 pb-4 rounded-sm w-1/2">
+                        <div className="bg-gray_fundo_mtfu pl-6 py-8 pr-4 pb-4 rounded-sm w-1/2">
                             <Typografy align="left" children="52" color="white" fontWeight={600} type="title" />
                             <Typografy align="left" children="Tasks completas" color="white" fontWeight={400} type="medium" />
                         </div>
@@ -70,12 +88,12 @@ export function ProjectDetail(){
                 
                 
 
-                <div className="p-4 w-2/3">
+                <div className="p-4 w-2/3 h-[7.5rem]">
                     <div className="flex gap-4">
                         <Typografy align="left" children="Fase:" color="white" fontWeight={400} type="medium" />
                         
                         <Select>
-                            <SelectTrigger className="w-1/6 bg-gray_fundo_sec_mtfu text-white
+                            <SelectTrigger className="w-2/6 bg-gray_fundo_sec_mtfu text-white
                              border-mtfu hover:bg-mtfu focus:ring-mtfu focus:ring-offset-3 h-[1.8rem]">
                                 <SelectValue placeholder="Selecione um projeto" />
                             </SelectTrigger>
@@ -87,7 +105,7 @@ export function ProjectDetail(){
                         </Select>
                     </div>
                     <div className="flex w-full  mt-2">
-                        <div className="w-2/3 flex flex-wrap gap-2">
+                        <div className="w-2/3 flex flex-wrap gap-2 cursor-default">
                             <div className=" bg-gray_fundo_mtfu pl-6 py-4 pr-4 pb-4 rounded-sm w-[49%]">
                                 <Typografy align="left" children="1" color="white" fontWeight={600} type="title" />
                                 <Typografy align="left" children="Total sprints" color="white" fontWeight={400} type="medium" />
@@ -110,7 +128,59 @@ export function ProjectDetail(){
                         <div className="ml-2 w-1/3">
                             <div className="bg-mtfu p-2 rounded-sm">
                                 <Typografy align="left" children="Conclusão da fase" color="white" fontWeight={400} type="medium" />
+
+                                <div className="w-full h-32 relative lg:h-32 sm:h-30 2xl:h-42 py-0 pr-6 flex items-center justify-center">
+                                  <ResponsiveContainer width="100%" height="100%">
+                                      <PieChart width={100} height={100} margin={{
+                                      top: 90,
+                                      right: 0,
+                                      left: 20,
+                                      bottom: 0,
+                                    }}>
+                                        <Pie
+                                          data={dataPie}
+                                          cx="50%"
+                                          cy="50%"
+
+                                          startAngle={180}
+                                            endAngle={0}
+                                            innerRadius={60}
+                                            outerRadius={80}
+
+                                          fill="#000000"
+                                          paddingAngle={0}
+                                          dataKey="value"
+
+                                        >
+                                         
+                                          {dataPie.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} stroke="#121212"/>
+                                          ))}
+                                        </Pie>
+                                        <Tooltip />
+                                      </PieChart>
+                                      
+                                  </ResponsiveContainer>
+                                  <div className="absolute mt-[2rem] ml-4 cursor-default">
+                                    <Typografy align="left" children={conclusionProject} color="white" fontWeight={600} type="medium" />
+                                  </div>
+                                  
+                                  
+                                </div>
+                                <div className="flex justify-between pl-2 pr-2">
+                                    <div className="flex gap-1 items-center">
+                                        <Status color="purple"></Status>
+                                        <Typografy align="left" children='Completo' color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                    <div className="flex gap-1 items-center">
+                                        <Status color="purple"></Status>
+                                        <Typografy align="left" children='Em progresso' color="white" fontWeight={400} type="footer" />
+                                    </div>
+                                    
+                                </div>
                             </div>
+
+                            
                         </div>
                     </div>
                     
