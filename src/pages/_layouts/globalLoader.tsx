de202da76edd9833b052ee6fb49db016789972e3
Jerônimo <jeronimo@testes-MacBook-Pro.local>
2024-10-04 06:32:34 -0300

    sync

--- src/pages/_layouts/globalLoader.tsx
+++ src/pages/_layouts/globalLoader.tsx
@@ -8,7 +8,7 @@ export function GlobalLoader(){
     if(!isFetching) return null;
 
     return(
-        <div className="bg-gray_fundo_sec_mtfu w-full absolute top-0 h-screen text-black flex items-center justify-center z-50
+        <div className="bg-gray_fundo_sec_mtfu w-full absolute top-0 h-screen text-black flex items-center justify-center z-[999]
         opacity-80">
             <CircleNotch size={60} className='animate-spin text-mtfu' />
         </div>
